e57cdbfacf7f549d849a4b9ff5399dbb072e84cd
Dev-Bastians <workingkeo@gmail.com>
2023-07-10 13:58:03 -0400

    add backend server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+
+const port = process.env.PORT || 8008;
+app.listen(port, () => console.log(`Server running on port ${port}`));
