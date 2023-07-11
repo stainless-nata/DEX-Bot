b29cde3ed5be165a8f276bf325d5eb99269df808
Dev-Bastians <workingkeo@gmail.com>
2023-07-11 18:10:13 -0400

    run blocknative sdk

--- routes/api/mempooltab.js
+++ routes/api/mempooltab.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs')
+const BlocknativeSDK = require('bnc-sdk')
 
 const router = express.Router();
 
@@ -10,6 +11,41 @@ const saveConfig = () => {
     if(err) console.log(err);
     console.log('Config Saved!');
   })
+}
+
+async function sdkSetup(sdk, configuration) {
+  const parsedConfiguration = typeof configuration === 'string' ? JSON.parse(configuration) : configuration
+  const globalConfiguration = parsedConfiguration.find(({ id }) => id === 'global')
+  const addressConfigurations = parsedConfiguration.filter(({ id }) => id !== 'global')
+
+  // save global configuration first and wait for it to be saved
+  globalConfiguration && await sdk.configuration({scope: 'global', filters: globalConfiguration.filters})
+
+  addressConfigurations.forEach(({id, filters, abi}) => {
+    const abiObj = abi ? { abi } : {}
+    sdk.configuration({...abiObj, filters, scope: id, watchAddress: true})
+  })
+}
+
+async function handleTransactionEvent(transaction) {
+  let tx = transaction.transaction;
+  console.log(tx.hash);
+}
+
+const scanMempool = async () => {
+  const blocknative = new BlocknativeSDK({
+    dappId: data.blockKey ,
+    networkId: 1,
+    transactionHandlers: [handleTransactionEvent],
+    ws: WebSocket,
+    onerror: (error) => {console.log(error)}
+  })
+
+  console.log(
+    chalk.red(`\nService Start ... `),
+  )
+
+  sdkSetup(blocknative, configuration)
 }
 
 router.post('/set', (req, res) => {
