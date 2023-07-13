4741aede7b4be6bddd42d6e843da0a10f447da8c
Dev-Bastians <workingkeo@gmail.com>
2023-07-12 21:28:01 -0400

    Refactor code structure

--- routes/api/mempooltab.js
+++ routes/api/mempooltab.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs')
-const BlocknativeSDK = require('bnc-sdk')
 
 const router = express.Router();
 
@@ -13,41 +12,6 @@ const saveConfig = () => {
   })
 }
 
-async function sdkSetup(sdk, configuration) {
-  const parsedConfiguration = typeof configuration === 'string' ? JSON.parse(configuration) : configuration
-  const globalConfiguration = parsedConfiguration.find(({ id }) => id === 'global')
-  const addressConfigurations = parsedConfiguration.filter(({ id }) => id !== 'global')
-
-  // save global configuration first and wait for it to be saved
-  globalConfiguration && await sdk.configuration({scope: 'global', filters: globalConfiguration.filters})
-
-  addressConfigurations.forEach(({id, filters, abi}) => {
-    const abiObj = abi ? { abi } : {}
-    sdk.configuration({...abiObj, filters, scope: id, watchAddress: true})
-  })
-}
-
-async function handleTransactionEvent(transaction) {
-  let tx = transaction.transaction;
-  console.log(tx.hash);
-}
-
-const scanMempool = async () => {
-  const blocknative = new BlocknativeSDK({
-    dappId: data.blockKey ,
-    networkId: 1,
-    transactionHandlers: [handleTransactionEvent],
-    ws: WebSocket,
-    onerror: (error) => {console.log(error)}
-  })
-
-  console.log(
-    chalk.red(`\nService Start ... `),
-  )
-
-  sdkSetup(blocknative, configuration)
-}
-
 router.post('/set', (req, res) => {
     console.log("Set Mempool environments")
 
@@ -55,7 +19,7 @@ router.post('/set', (req, res) => {
     config.filterAmount = req.body.filterAmount
     config.filter = req.body.action
 
-    saveConfig()
+    // saveConfig()
     res.json('Success');
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors')
 var bodyParser = require('body-parser')
+const BlocknativeSDK = require('bnc-sdk')
+const WebSocket = require('ws')
+const chalk = require('chalk')
+const fs = require('fs')
+require('dotenv').config();
 
 const maintab = require('./routes/api/maintab');
 const mempooltab = require('./routes/api/mempooltab');
@@ -14,6 +19,43 @@ app.options("*", cors());
 app.use('/maintab', maintab);
 app.use('/mempooltab', mempooltab);
 
+
+
+async function sdkSetup(sdk, configuration) {
+    const parsedConfiguration = typeof configuration === 'string' ? JSON.parse(configuration) : configuration
+    const globalConfiguration = parsedConfiguration.find(({ id }) => id === 'global')
+    const addressConfigurations = parsedConfiguration.filter(({ id }) => id !== 'global')
+  
+    // save global configuration first and wait for it to be saved
+    globalConfiguration && await sdk.configuration({scope: 'global', filters: globalConfiguration.filters})
+  
+    addressConfigurations.forEach(({id, filters, abi}) => {
+      const abiObj = abi ? { abi } : {}
+      sdk.configuration({...abiObj, filters, scope: id, watchAddress: true})
+    })
+}
+  
+async function handleTransactionEvent(transaction) {
+    let tx = transaction.transaction;
+    console.log(tx.hash);
+}
+  
+const scanMempool = async () => {
+    const blocknative = new BlocknativeSDK({
+        dappId: process.env.BLOCKNATIVE_KEY,
+        networkId: 1,
+        transactionHandlers: [handleTransactionEvent],
+        ws: WebSocket,
+        onerror: (error) => {console.log(error)}
+    })
+    console.log(
+        chalk.red(`\nMempool Scanner Started ... `),
+    )
+    const configuration = JSON.parse(fs.readFileSync('configuration.json', 'utf-8'))
+    sdkSetup(blocknative, configuration)
+}
+
+// scanMempool()
 
 const port = process.env.PORT || 8008;
 app.listen(port, () => console.log(`Server running on port ${port}`));
